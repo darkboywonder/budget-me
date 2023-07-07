b4e5a139bf9c3c1a9f6acde2242f1d455f4c7de6
habtl1 <lhabtemichael1@gmail.com>
2023-07-07 18:44:02 -0500

    expense list

--- src/frontend/src/components/ExpenseList.vue
+++ src/frontend/src/components/ExpenseList.vue
@@ -1,5 +1,30 @@
+<script>
+import ExpenseList from './ExpenseList.vue';
 
+export default {
+  components: {
+    ExpenseList
+  },
+}
+</script>
 
 data() {
-
-    return {
+  return {
+    expenses: [
+      { id: 1, description: 'Groceries', amount: 50, category: 'Food' },
+      { id: 2, description: 'Gas', amount: 30, category: 'Transportation' },
+      { id: 3, description: 'Dinner', amount: 40, category: 'Food' },
+      // Add more expense items as needed
+    ]
+  };
+}
+<template>
+  <div>
+    <h2>Expense List</h2>
+    <ul>
+      <li v-for="expense in expenses" :key="expense.id">
+        {{ expense.description }} - ${{ expense.amount }} ({{ expense.category }})
+      </li>
+    </ul>
+  </div>
+</template>
