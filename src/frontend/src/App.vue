a9668fe949713d4b11229056c58c0e60f5f36acc
prints-philbert <119360081+prints-philbert@users.noreply.github.com>
2023-07-07 19:08:19 -0500

    "first component test: display template"

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <hello-world/>
+  <Expense-List/>
 </template>
 
 <script>
 import HelloWorld from './components/Example.vue'
+import ExpenseList from './components/ExpenseList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    ExpenseList
   }
 }
 </script>
--- src/frontend/src/components/ExpenseList.vue
+++ src/frontend/src/components/ExpenseList.vue
@@ -1,23 +1,3 @@
-<script>
-import ExpenseList from './ExpenseList.vue';
-
-export default {
-  components: {
-    ExpenseList
-  },
-}
-</script>
-
-data() {
-  return {
-    expenses: [
-      { id: 1, description: 'Groceries', amount: 50, category: 'Food' },
-      { id: 2, description: 'Gas', amount: 30, category: 'Transportation' },
-      { id: 3, description: 'Dinner', amount: 40, category: 'Food' },
-      // Add more expense items as needed
-    ]
-  };
-}
 <template>
   <div>
     <h2>Expense List</h2>
@@ -28,3 +8,22 @@ data() {
     </ul>
   </div>
 </template>
+
+
+<script>
+export default {
+  name: 'ExpenseList',
+    data() {
+      return {
+        expenses: [
+          { id: 1, description: 'Groceries', amount: 50, category: 'Food' },
+          { id: 2, description: 'Gas', amount: 30, category: 'Transportation' },
+          { id: 3, description: 'Dinner', amount: 40, category: 'Food' },
+          // Add more expense items as needed
+        ]
+      }
+    }
+}
+</script>
+
+
