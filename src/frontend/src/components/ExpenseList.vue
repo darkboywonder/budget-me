00ec20f87377b0c990e449fc03882b9707bbb568
habtl1 <lhabtemichael1@gmail.com>
2023-07-15 17:45:12 -0500

    Edited styling

--- src/frontend/src/components/ExpenseList.vue
+++ src/frontend/src/components/ExpenseList.vue
@@ -36,6 +36,7 @@ export default {
       }
     };
   },
+
   methods: {
     addExpense() {
       // Generate a unique ID for the new expense
@@ -57,4 +58,60 @@ export default {
     }
   }
 };
+</script>
+<style>
+  body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+  }
 
+  div {
+    max-width: 400px;
+    margin: 0 auto;
+    padding: 20px;
+  }
+
+  h2 {
+    margin-bottom: 20px;
+    text-align: center;
+  }
+
+  form {
+    margin-bottom: 20px;
+  }
+
+  label {
+    display: block;
+    margin-bottom: 5px;
+  }
+
+  input[type="text"],
+  input[type="number"] {
+    width: 100%;
+    padding: 5px;
+    margin-bottom: 10px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+  }
+
+  button[type="submit"] {
+    display: block;
+    width: 100%;
+    padding: 10px;
+    background-color: #4caf50;
+    color: white;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    margin-bottom: 5px;
+  }
+</style>
